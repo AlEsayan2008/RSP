45420d2782ddcd694731a4e6eb11d409d9c2016b
AlEsayan2008 <167086118+AlEsayan2008@users.noreply.github.com>
2024-04-26 17:05:41 +0400

    rsp.py

--- rsp.py
+++ rsp.py
@@ -1,43 +1,50 @@
-#rsp game:
+import json
 import random 
+import logging
 
-# r>s, r<p, s>p
-Choice = {"rock": "r", "scissors": "s", "paper": "p"}
-choice_variants = list(Choice.values())
+
+
+def read_json(source: str):   
+    try:
+        with open(source, "r", encoding="utf-8") as read_file:
+            data = json.load(read_file)
+        return data
+    except FileNotFoundError:
+        logging.error(f"{source} file not found")
+        raise FileNotFoundError
+    
+
+data = read_json("RSP/choice.json")
+rsp = data["choice"]
+
 
 
 def command():
     return input("\nPlease enter command 'play' for playing.\n")
 
-
-
-
 def you_win(your_choice, computer):
     if (your_choice == "r" and computer == "s") or (your_choice == "p" and computer == "r") or (your_choice == "s" and computer == "p"):
         return True
-   
+
 def its_tie(your_choice, computer):
-     if (your_choice == "r" and computer == "r") or (your_choice == "p" and computer == "p") or (your_choice == "s" and computer == "s"):
+    if (your_choice == "r" and computer == "r") or (your_choice == "p" and computer == "p") or (your_choice == "s" and computer == "s"):
         return True
 
-
-
 def play_rsp():
-    if command().lower() == "play":
-        if your_choice == "r" or your_choice == "s" or your_choice == "p":
-            if you_win(your_choice, computer):
-                print(f"You win.Computer choice is {computer}")
-            elif its_tie(your_choice, computer):
-                print(f"It's draw.Computer choice is {computer}") 
+    while True:
+        if command().lower() == "play":
+            your_choice = input("\nEnter your choice\n")
+            if your_choice == "r" or your_choice == "s" or your_choice == "p":
+                computer = random.choice(rsp)
+                if you_win(your_choice, computer):
+                    print(f"You win. Computer choice is {computer}")
+                elif its_tie(your_choice, computer):
+                    print(f"It's a draw. Computer choice is {computer}") 
+                else:
+                    print(f"You lose. Computer choice is {computer}")
             else:
-                print(f"You lose. Computer choice is {computer}")
+                print("Wrong Choice !!!")
         else:
-            print("Wrong Choice !!!")
-    else:
-        print("CommandError !!!")
+            print("Command Error !!!")
 
-
-
-your_choice = input("\nEnter your choice\n")
-computer = random.choice(choice_variants)    
 play_rsp()
